cbadb6ae442d2cf51094075b6b2fe932fc530076
bdnvkomali <bdnvkomali@gmail.com>
2024-07-22 23:55:03 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,7 +1,7 @@
-#include "Soundex.h"
+#include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-#include <gtest/gtest.h>
+
 
  
 char getSoundexCode(char c) {
@@ -21,78 +21,25 @@ char fetch_firstchar(const std::string& name) {
     return toupper(name[0]);
 }
  
-void appendSoundex(std::string& soundex, char code, char& prevCode) {
-    if (code != '0' && code != prevCode) {
-        soundex += code;
-        prevCode = code;
+void padSoundex(char* soundex) {
+    size_t len = strlen(soundex);
+    while (len < 4) {
+        soundex[len++] = '0';
     }
-}
- 
-std::string initializeSoundex(const std::string& name, char firstChar) {
-    std::string soundex(1, firstChar);
-    char secondex = getSoundexCode(name[1]);
-    if (secondex != '0') {
-        soundex += secondex;
-    }
-    return soundex;
-}
- 
-std::string processSoundex(const std::string& name, char firstChar) {
-   std::string soundex = initializeSoundex(name, firstChar);
-    char prevCode = soundex[1];
-    for (size_t i = 2; i < name.length() && soundex.length() < 4; ++i) 
-    {
-        char code = getSoundexCode(name[i]);
-        appendSoundex(soundex, code, prevCode);
-    }
- 
-    return soundex;
-}
- 
-std::string paddingSoundex(const std::string& soundex) {
-    std::string paddedSoundex = soundex;
-    paddedSoundex.resize(4, '0');
-    return paddedSoundex;
-}
- 
-std::string generateSoundex(const std::string& name) {
-    if (name.empty()) return "";
- 
-    char firstChar = fetch_firstchar(name);
-    std::string processedName = processSoundex(name, firstChar);
-    std::string paddedSoundex = paddingSoundex(processedName);
- 
-    return paddedSoundex;
-}
- 
-TEST(SoundexTest, BasicTest) {
-    EXPECT_EQ(generateSoundex("Singh"), "S520");
-    EXPECT_EQ(generateSoundex("Kumar"), "K560");
-    EXPECT_EQ(generateSoundex("Sharma"), "S650");
-    EXPECT_EQ(generateSoundex("Patel"), "P340");
-    EXPECT_EQ(generateSoundex("Gupta"), "G130");
-}
- 
-TEST(SoundexTest, EdgeCases) {
-    EXPECT_EQ(generateSoundex("Chand"), "C530");
-    EXPECT_EQ(generateSoundex("Mehra"), "M600");
-    EXPECT_EQ(generateSoundex("Pandey"), "P530");
-    EXPECT_EQ(generateSoundex(""), "");
-}
- 
-TEST(SoundexTest, CaseInsensitive) {
-    EXPECT_EQ(generateSoundex("SINGH"), "S520");
-    EXPECT_EQ(generateSoundex("kUmAr"), "K560");
-    EXPECT_EQ(generateSoundex("shARmA"), "S650");
-}
- 
-TEST(SoundexTest, SingleCharacterName) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
-    EXPECT_EQ(generateSoundex("B"), "B000");
-}
- 
-TEST(SoundexTest, AllVowels) {
-    EXPECT_EQ(generateSoundex("Aeio"), "A000");
-    EXPECT_EQ(generateSoundex("Euio"), "E000");
+    soundex[4] = '\0';
 }
 
+ 
+void initializeSoundex(char* soundex, char firstChar) {
+    soundex[0] = toupper(firstChar);
+void generateSoundex(const char* name, char* soundex) {
+    initializeSoundex(soundex, name[0]); // Initialize soundex with the first character of name
+    char prevCode = getSoundexCode(name[0]); // Get Soundex code for the first character
+ 
+    size_t i = 1;
+    while (name[i] != '\0' && i < 4) {
+        prevCode = eliminateZeroAndRepeatedValue(prevCode, soundex, i, name);
+        i++;
+    }
+    padSoundex(soundex);
+}
